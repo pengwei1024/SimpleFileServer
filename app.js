ac5c1d975a444b16f1c2c23623664388ed26d31d
舞影凌风 <pengwei1024@gmail.com>
2022-05-28 12:56:46 +0800

    设置上传限制及下载文件名

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 
 var express = require('express'),
 config = require('./config/config');
-const bodyParser = require('body-parser')
 
 var app = express();
 
@@ -21,8 +20,8 @@ module.exports = require('./config/express')(app, config);
 //     throw new Error(e);
 //   });
 
-app.use(bodyParser.urlencoded({ limit: '500mb', extended: true }))
-app.use(bodyParser.json({limit: '500mb'}))
+app.use(express.urlencoded({ limit: '500mb', extended: true }))
+app.use(express.json({limit: '500mb'}))
 
 app.listen(config.port, function () {
   console.log('Express server listening on port ' + config.port);
